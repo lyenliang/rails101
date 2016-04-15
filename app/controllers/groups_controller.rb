a5d71286f11bcfddf4056901a362aaee5c3a672b
lyenliang <lyenliang@gmail.com>
2016-04-15 14:10:48 +0800

    Add comments

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,15 +1,18 @@
 class GroupsController < ApplicationController
+  # 列出所有的討論版，可以選擇各個單版
   def index
     @groups = Group.all
   end
 
+  # 裡面會有表單，填完以後可以送出
   def new
     @group = Group.new
   end
 
+  # new 送出來的表單到 create 這個 action，新增一筆資料
   def create
     @group = Group.create(group_params)
-
+    
     if @group.save
       redirect_to groups_path
     else
@@ -17,6 +20,7 @@ class GroupsController < ApplicationController
     end
   end
 
+  # edit 送出來的表單可以到 update 這個 action，更新該筆資料
   def update
     @group = Group.find(params[:id])
 
@@ -27,14 +31,17 @@ class GroupsController < ApplicationController
     end
   end
 
+  # 顯示討論版版名跟簡介
   def show
     @group = Group.find(params[:id])
   end
 
+  # 裡面會有表單呈現現有資料來，填完資料後可以送出
   def edit
     @group = Group.find(params[:id])
   end
 
+  # 送出刪除請求，刪除該筆資料
   def destroy
     @group = Group.find(params[:id])
     @group.destroy
