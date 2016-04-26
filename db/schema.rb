cf77c6613fdcf23604d2385c45c0b7e49b148168
lyenliang <lyenliang@gmail.com>
2016-04-26 11:06:10 +0800

    Use "posts_count" instead of executing MySQL to get the count of posts in a group.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425083246) do
+ActiveRecord::Schema.define(version: 20160426024748) do
 
   create_table "group_users", force: :cascade do |t|
     t.integer  "group_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160425083246) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.integer  "posts_count"
   end
 
   create_table "posts", force: :cascade do |t|
